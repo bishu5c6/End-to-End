895a542c9f7f674fae14ec2d208d8d86efb449c7
Biswanth <pinnikabiswanth@gmail.com>
2024-05-11 14:04:56 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,68 +1,34 @@
-import setuptools
-
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-
-__version__ = "0.0.0"
-
-REPO_NAME = "End-to-end-ML-Project-Implementation"
-AUTHOR_USER_NAME = "bishu5c6"
-SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "pinnikabiswanth@gmail.com"
-
-
-setuptools.setup(
-    name=SRC_REPO,
-    version=__version__,
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description="A small python package for ml app",
-    long_description=long_description,
-    long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
-)
-
-
-
-
-
-'''
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 from typing import List
 
-PROJECT_NAME ="WINE_QUALITY_PREDICTION"
+PROJECT_NAME ="Wine_Quality_Prediction"
 VERSION = "0.0.1"
 DESCRIPTION = "END TO END ML PROJECT"
-AUTHOR = "BISWANTH PINNIKA"
+AUTHOR = "bishu5c6"
 AUTHOR_EMAIL = "pinnikabiswanth@gmail.com"
 
 REQUIREMENTS_FILE_NAME = 'requirements.txt'
 
+
 HYPHON_E_DOT = "-e ."
 
-def get_requirements_list() -> List[str]:
-    with open(REQUIREMENTS_FILE_NAME) as requriment_file:
-        requriment_list = requriment_file.readlines()
-        requriment_list = [ requriment_name.replace("\n", "") for requriment_name in requriment_list]
+def get_requriments(filepath: str) -> List[str]:
+    requirements = []
 
-        if HYPHON_E_DOT in requriment_list:
-            requriment_list.remove(HYPHON_E_DOT)
-        return requriment_list
+    with open(filepath) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [ i.replace("\n", "") for i in requirements]
+
+        if HYPHON_E_DOT in requirements:
+            requirements.remove(HYPHON_E_DOT)
 
 
-setup(name=PROJECT_NAME,
+setup(name=PROJECT_NAME, 
       version=VERSION,
       description=DESCRIPTION,
       author=AUTHOR,
       author_email=AUTHOR_EMAIL,
+      url = f"https://github.com/{AUTHOR}",
       packages=find_packages(),
-      install_requires = get_requirements_list()
+      install_requires = get_requriments("requirements.txt")
      )
-    
-'''
