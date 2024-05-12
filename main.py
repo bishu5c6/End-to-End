0dc748a507a9985f5c2e5f23e7f04990f14776ed
Biswanth <pinnikabiswanth@gmail.com>
2024-05-12 17:21:30 +0530

    data validation completd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.Wine_Quality_Prediction import logger
 from src.Wine_Quality_Prediction.exception import CustomException
 from src.Wine_Quality_Prediction.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
+from src.Wine_Quality_Prediction.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
 
 
 
@@ -8,6 +9,17 @@ STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise CustomException(e, sys)
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
