d80ac632899f91c49d2f3db000cb02a260cd8450
Biswanth <pinnikabiswanth@gmail.com>
2024-05-22 16:49:53 +0530

    model trainer and evaluation completed

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from src.Wine_Quality_Prediction.exception import CustomException
 from src.Wine_Quality_Prediction.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from src.Wine_Quality_Prediction.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
 from src.Wine_Quality_Prediction.pipeline.stage_3_data_transformation import DataTransformationTrainingPipeline
-
+from src.Wine_Quality_Prediction.pipeline.stage_4_model_trainer import ModelTrainerTrainingPipeline
+from src.Wine_Quality_Prediction.pipeline.stage_5_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -30,6 +31,27 @@ STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise CustomException(e, sys)
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise CustomException(e, sys)
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
